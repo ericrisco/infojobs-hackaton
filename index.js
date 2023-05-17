5eadf7f31913e06281e8a4e5dbf86c23e7f0af30
Eric Risco de la Torre <erisco@icloud.com>
2023-05-18 01:30:25 +0200

    [ADD] index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const connectDB = require('./db/db');
+
+const bot = require('./bot/bot');
+const startCommand = require('./bot/commands/start');
+const errorCommand = require('./bot/commands/error');
+
+app.get('/ping', (req, res) => {
+    res.json({ success: true });
+});
+
+startCommand(bot);
+errorCommand(bot);
+
+app.listen(port, async () => {
+    await connectDB();
+    console.log(`Server running on http://localhost:${port}`);
+});
